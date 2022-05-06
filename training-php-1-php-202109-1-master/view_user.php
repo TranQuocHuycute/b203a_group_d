183906ce9a4d71a366ae200d69356230596d493a
TranQuocHuya <86847384+TranQuocHuya@users.noreply.github.com>
2022-05-06 22:17:20 +0700

    add cost for user

--- training-php-1-php-202109-1-master/view_user.php
+++ training-php-1-php-202109-1-master/view_user.php
@@ -8,7 +8,7 @@ $id = NULL;
 if (!empty($_GET['id'])) {
     $id = $_GET['id'];
     $user = $userModel->findUserById($id);//Update existing user
-   
+    $a = $userModel -> viewBanks($id);
 }
 
 
@@ -50,6 +50,11 @@ if (!empty($_POST['submit'])) {
             <div class="form-group">
                 <label for="password">Email</label>
                 <span><?php if (!empty($user[0]['name'])) echo $user[0]['email'] ?></span>
+            </div>
+            <div class="form-group">
+                <label for="password">COST</label>
+                <span><?php if( !empty ( $a[0]['cost'])) { echo '<h4 style = " color:red">'. $a[0]['cost'] . '  VND </h4>' ;} 
+                else  echo '  <h4 style = " color:red"> Không có tiền :(( </h4>'; ?></span>
             </div>
         </form>
     <?php } else { ?>
